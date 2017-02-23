cb252ef581dd398c73ef527da2aef5cde4fc525c
Charlie Jackson <charlie@charliejackson.com>
2017-02-23 12:50:13 +0000

    Error if the module is out of date

--- bin/cj.js
+++ bin/cj.js
@@ -6,6 +6,7 @@ const path = require('path');
 const spawn = require('child_process').spawnSync;
 const commandLineCommands = require('command-line-commands');
 const generatorMap = require('../generator-map.json');
+const npmCheck = require('npm-check');
 
 winston.cli();
 
@@ -13,80 +14,107 @@ const validCommands = [null, 'install', 'update'];
 const { command, argv } = commandLineCommands(validCommands);
 const windowsEnvironment = /^win/.test(process.platform);
 
-if (argv.length && argv[argv.length - 1] === '--debug') {
+const args = argv;
+
+if (args.length && args[args.length - 1] === '--debug') {
   winston.level = 'debug';
+  args.pop();
 }
 
 winston.log('debug', 'Command: ', command);
-winston.log('debug', 'Args: ', argv);
+winston.log('debug', 'Args: ', args);
 
-if (command === null) {
-  const optionDefinitions = [
-    { name: 'version', type: Boolean },
-    { name: 'debug', type: Boolean }
-  ];
+winston.log('debug', 'Getting global packages to check module version');
 
-  winston.log('debug', 'No command');
+npmCheck({
+  global: true,
+}).then((currentState) => {
+  const globalPackages = currentState.get('packages');
 
-  const options = commandLineArgs(optionDefinitions, argv);
-
-  if (options.version) {
-    winston.log('debug', 'Get version');
-    winston.log('info', 'Version 1.0.0');
-  }
-}
-
-if (command === 'install' && argv[0]) {
-  winston.log('debug', 'Install');
-
-  const yo = path.join(__dirname, '/../node_modules/yo/lib/cli.js');
-
-  winston.log('debug', 'Yo path', yo);
-  winston.log('debug', 'Spawn Yeoman child process');
-
-  let generatorName = argv[0];
-
-  if (generatorMap[generatorName]) {
-    generatorName = generatorMap[generatorName];
-  }
-
-  const generatorPath = `./../node_modules/generator-${generatorName}/generators/app/index.js`;
-  const generator = path.join(__dirname, generatorPath);
-
-  winston.log('debug', 'Generator path: ', generator);
-
-  spawn('node', [yo, generator], { stdio: 'inherit' });
-
-  winston.log('debug', 'Finished Yeoman child process');
-} else if (command === 'update') {
-  winston.log('debug', 'Update');
-
-  const update = path.join(__dirname, '/../node_modules/npm-check-updates/bin/npm-check-updates');
-
-  winston.log('debug', 'npm-check-updates path', update);
-
-  if (argv[0] === '--self' || argv[0] === '-s') {
-    winston.log('debug', 'Update self');
-    winston.log('debug', 'Spawn npm-check-updates child process');
-
-    let npm;
-
-    if (windowsEnvironment) {
-      npm = 'npm.cmd';
-    } else {
-      npm = 'npm';
+  globalPackages.forEach((globalPackage) => {
+    if (globalPackage.moduleName !== 'cj-cmd') {
+      return false;
     }
 
-    spawn(npm, ['install', '-g', 'cj-cmd'], { stdio: 'inherit' });
-  } else {
-    const updateArgs = argv;
+    if (globalPackage.latest === globalPackage.installed) {
+      winston.log('debug', 'cj-cmd is up to date');
+      return true;
+    }
 
-    updateArgs.unshift(update);
-    updateArgs.push('-u');
+    const errorMessage = 'cj-cmd is out of date! You must upgrade the plugin using: cj update --self';
 
-    winston.log('debug', 'Spawn npm-check-updates child process');
-    spawn('node', updateArgs, { stdio: 'inherit' });
+    winston.log('error', errorMessage);
+    throw new Error(errorMessage);
+  });
+
+  if (command === null) {
+    const optionDefinitions = [
+      { name: 'version', type: Boolean },
+      { name: 'debug', type: Boolean }
+    ];
+
+    winston.log('debug', 'No command');
+
+    const options = commandLineArgs(optionDefinitions, args);
+
+    if (options.version) {
+      winston.log('debug', 'Get version');
+      winston.log('info', 'Version 1.0.0');
+    }
   }
 
-  winston.log('debug', 'Finished update child process');
-}
+  if (command === 'install' && args[0]) {
+    winston.log('debug', 'Install');
+
+    const yo = path.join(__dirname, '/../node_modules/yo/lib/cli.js');
+
+    winston.log('debug', 'Yo path', yo);
+    winston.log('debug', 'Spawn Yeoman child process');
+
+    let generatorName = args[0];
+
+    if (generatorMap[generatorName]) {
+      generatorName = generatorMap[generatorName];
+    }
+
+    const generatorPath = `./../node_modules/generator-${generatorName}/generators/app/index.js`;
+    const generator = path.join(__dirname, generatorPath);
+
+    winston.log('debug', 'Generator path: ', generator);
+
+    spawn('node', [yo, generator], { stdio: 'inherit' });
+
+    winston.log('debug', 'Finished Yeoman child process');
+  } else if (command === 'update') {
+    winston.log('debug', 'Update');
+
+    const update = path.join(__dirname, '/../node_modules/npm-check-updates/bin/npm-check-updates');
+
+    winston.log('debug', 'npm-check-updates path', update);
+
+    if (args[0] === '--self' || args[0] === '-s') {
+      winston.log('debug', 'Update self');
+      winston.log('debug', 'Spawn npm-check-updates child process');
+
+      let npm;
+
+      if (windowsEnvironment) {
+        npm = 'npm.cmd';
+      } else {
+        npm = 'npm';
+      }
+
+      spawn(npm, ['install', '-g', 'cj-cmd'], { stdio: 'inherit' });
+    } else {
+      const updateArgs = args;
+
+      updateArgs.unshift(update);
+      updateArgs.push('-u');
+
+      winston.log('debug', 'Spawn npm-check-updates child process');
+      spawn('node', updateArgs, { stdio: 'inherit' });
+    }
+
+    winston.log('debug', 'Finished update child process');
+  }
+});
